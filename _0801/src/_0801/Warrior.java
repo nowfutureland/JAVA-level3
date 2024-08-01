53b63f1dd84543f27432985ba44dd4cde12d43c2
nowfutureland <126860322+nowfutureland@users.noreply.github.com>
2024-08-01 15:09:48 +0900

    Add files via upload

---
+++ _0801/src/_0801/Warrior.java
@@ -0,0 +1,63 @@
+package _0801;
+
+public class Warrior extends Hero {
+	
+	int 분노;
+	
+	public Warrior(String 이름, int 체력, int 공격력) {
+		super(이름, 체력, 공격력);
+		공격력 = 공격력계산();
+		분노 = 30;
+		속성 = "불";
+	}
+	
+	int 공격력계산(){
+		return 공격력 + 10;
+	}
+	
+	@Override
+	void 자기소개(){
+		super.자기소개();
+		System.out.printf("분노:%s | 속성:%s\n", 분노,속성);
+	}
+
+	@Override
+	void 공격(Monster 적) {
+		자기소개(); 적.자기소개();
+		
+		System.out.printf("%s ▷ %s 공격!\n", 이름,적.이름);
+		적.체력 -= 공격력;
+		분노 += 10;
+		
+		자기소개(); 적.자기소개();
+	}
+
+	@Override
+	void 특수공격(Monster 적) {
+		자기소개(); 적.자기소개();
+		
+		int 특수공격력;
+		
+		if(분노 >= 100) {
+			특수공격력 = 공격력*2; 
+			분노 = 0;
+		}else {
+			특수공격력 = 공격력;
+		}
+		
+		if(적.속성.equals("풀")) {
+			System.out.printf("풀에 불이 불어 효과가 굉장했다\n");
+			System.out.printf("불불불불불불불불불불불불불불불불불\n");
+			System.out.printf("%s ▷ %s 공격!\n", 이름,적.이름);
+			적.체력 -= 특수공격력*3;
+		}else {
+			System.out.printf("%s ▷ %s 공격!\n", 이름,적.이름);
+			적.체력 -= 특수공격력*2;
+			
+		}
+		
+		자기소개(); 적.자기소개();
+		
+	}
+
+}
